c0db62f06197e1ef4048e5eff5560f5af92b8aff
GamerJay1212 <mastercrafterhayden@outlook.com>
2024-09-04 19:53:55 -0500

    Changed a couple things

---
+++ Main code.py
@@ -0,0 +1,5 @@
+while True:
+    print("type HELP for help")
+    input1 = input("Command> ")
+    if input1 == "HELP":
+        print("HELP - Shows this list")
--- New Text Document.py
+++
@@ -1,2 +0,0 @@
-while True:
-    print("type HELP for help")
